58e45243f6a8ea02811e34ea6251311ee4293ed6
lizchiduku <50470205+lizchiduku@users.noreply.github.com>
2019-08-11 23:08:45 -0500

    Update Application.java

--- string/src/main/java/com/codingdojo/spring/Application.java
+++ string/src/main/java/com/codingdojo/spring/Application.java
@@ -1,13 +1,22 @@
-package com.codingdojo.spring;
+package com.codingdojo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+@RestController
+public class StringApplication {
+public static void main(String[] args) {
+	SpringApplication.run(StringApplication.class, args);
 	}
-
+	@RequestMapping("/")
+	public String Hi() {
+		return "Hello client! How are you doing?";
+	}
+	@RequestMapping("/random")
+	public String spring() {
+		return "Spring Boot is great! So easy to just respond with strings";
+	}
 }
